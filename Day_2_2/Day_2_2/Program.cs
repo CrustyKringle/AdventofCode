15ec6cae419350ea9a6c1bc8aa2709b363b5fb9e
CrustyKringle <124091355+CrustyKringle@users.noreply.github.com>
2023-12-20 10:50:29 +0100

    Add files via upload

---
+++ Day_2_2/Day_2_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        var lines = File.ReadAllLines(@"C:\Users\jenst\Desktop\JENS_AO_23_24\AdventOfCode\Day_2_1\Input_Day_2.txt");
+        var productSum = 0;
+
+        foreach (var line in lines)
+        {
+            var colorValues = Regex.Matches(line, @"(\d+)\s*([a-zA-Z]+)")
+                .Select(match => (
+                    Value: int.Parse(match.Groups[1].Value),
+                    Color: match.Groups[2].Value.ToLower()
+                ));
+
+            var maxValues = colorValues
+                .GroupBy(pair => pair.Color)
+                .Select(group => group.Max(pair => pair.Value));
+
+            var product = maxValues.Aggregate(1, (acc, value) => acc * value);
+            productSum += product;
+
+            Console.WriteLine($"Game {GetGameId(line)}: {product}");
+        }
+        Console.WriteLine($"Sum: " + productSum);
+    }
+
+    
+    static int GetGameId(string line)
+    {
+        var idMatch = Regex.Match(line, @"Game (\d+)");
+        return idMatch.Success ? int.Parse(idMatch.Groups[1].Value) : -1;
+    }
+}
